906a69b3222ba43157f69f8df7297047efe8b9b1
RentoSaijo <rentosaijo0527@gmail.com>
2025-08-05 20:00:19 +0900

    Get all games from 1993-94 to 2024-25

--- collect.R
+++ collect.R
@@ -1,4 +1,45 @@
 # Load necessary libraries.
-library(nhlscraper)
 suppressMessages(library(tidyverse))
+library(nhlscraper)
+library(lubridate)
+library(stringr)
 
+# Set helper functions.
+to_espn_date <- function(date) {
+  date <- ymd_hms(date, tz='UTC')
+  as.integer(format(date, '%Y%m%d'))
+}
+get_espn_event_id <- function(href) {
+  event <- str_extract(href, '(?<=/events/)\\d+')
+  as.integer(event)
+}
+get_espn_all_events <- function(all_seasons) {
+  all_seasons %>% 
+    select(
+      season=id,
+      espn_start_date,
+      espn_end_date
+    ) %>% 
+    pmap_dfr(function(season, espn_start_date, espn_end_date) {
+      events <- get_espn_events(espn_start_date, espn_end_date)
+      if (nrow(events)==0) {
+        return(tibble(season=integer(), id=integer()))
+      }
+      tibble(season=season, id=get_espn_event_id(events$`$ref`))
+    })
+}
+
+# Get all seasons from 1993-1994 to 2024-2025.
+all_seasons <- get_seasons() %>% 
+  mutate(
+    espn_start_date=to_espn_date(startDate),
+    espn_end_date=to_espn_date(endDate)
+  ) %>% 
+  filter(id<=20242025) %>% 
+  arrange(id)
+
+# Get all games and events from 1993-1994 to 2024-2025.
+all_games <- get_games() %>% 
+  filter(season>=19931994) %>% 
+  filter(season<=20242025)
+all_events <- get_espn_all_events(all_seasons)
