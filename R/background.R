1ea28ef5a7e024f7c0056bc07e1d1a409f780d90
RentoSaijo <rentosaijo0527@gmail.com>
2025-08-06 15:38:01 +0900

    Collect ESPN pbps

--- R/background.R
+++ R/background.R
@@ -1,12 +1,66 @@
+# Load libraries.
 suppressMessages(library(tidyverse))
+library(nhlscraper)
 library(jsonlite)
-espn_pbps_copy <- espn_pbps
-espn_pbps_flat <- espn_pbps_copy %>% 
-  mutate(
-    participants=map_chr(participants, function(x) {
-      if (length(x)==0) {
-        return(NA_character_)
+
+# Read data.
+games <- read.csv('data/games.csv')
+events <- read.csv('data/events.csv')
+
+# Set helpers.
+get_espn_all_pbps <- function(events) {
+  events %>% 
+    pmap_dfr(function(season, id) {
+      pbp <- get_espn_event_play_by_play(id)
+      if (nrow(pbp)==0) {
+        return(tibble(
+          season=integer(), 
+          event=integer(),
+          id=character(),
+          sequence=character(),
+          text=character(),
+          away_score=integer(),
+          home_score=integer(),
+          participants=list(),
+          type=character(),
+          period=integer(),
+          clock=integer(),
+          x=integer(),
+          y=integer(),
+          team=character(),
+          strength=character()
+        ))
       }
-      toJSON(x, auto_unbox=TRUE)
+      tibble(
+        season=season,
+        event=id,
+        id=pbp$id,
+        sequence=pbp$sequenceNumber,
+        text=pbp$text,
+        away_score=pbp$awayScore,
+        home_score=pbp$homeScore,
+        participants=pbp$participants,
+        type=pbp$`type.text`,
+        period=pbp$`period.number`,
+        clock=pbp$`clock.value`,
+        x=pbp$`coordinate.x`,
+        y=pbp$`coordinate.y`,
+        team=pbp$`team.$ref`,
+        strength=pbp$`strength.text`
+      )
     })
-  )
+}
+flatten_espn_participants <- function(espn_pbps) {
+  espn_pbps %>% 
+    mutate(
+      participants=map_chr(participants, function(x) {
+        if (length(x)==0) {
+          return(NA_character_)
+        }
+        toJSON(x, auto_unbox=TRUE)
+      })
+    )
+}
+
+# Get ESPN play-by-plays.
+espn_pbps <- get_espn_all_pbps(events)
