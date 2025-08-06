0b616231f9edf2533901d52bf03f13d67340a1f1
RentoSaijo <rentosaijo0527@gmail.com>
2025-08-06 15:38:56 +0900

    Encode shot type

--- R/shots.R
+++ R/shots.R
@@ -10,6 +10,18 @@ play_type_to_goal_boolean <- function(play_type) {
     TRUE~NA_integer_
   )
 }
+encode_shot_type <- function(text) {
+  case_when(
+    grepl('Wrist', text)~'wrist',
+    grepl('Snap', text)~'snap',
+    grepl('Slap', text)~'slap',
+    grepl('Tip-In', text)~'tip',
+    grepl('Backhand', text)~'backhand',
+    grepl('Wrap-around', text)~'wrap',
+    grepl('Batted', text)~'batted',
+    TRUE~'other'
+  )
+}
 
 # Read data.
 test <- get_espn_event_play_by_play(401777460)
@@ -22,14 +34,18 @@ test_flat <- test %>%
       toJSON(x, auto_unbox=TRUE)
     })
   )
+
 espn_pbps <- read.csv('data/espn_pbps.csv')
 
 # Only leave shots.
 test_shots <- test_flat %>% 
   filter(type.text=='Shot' | type.text=='Goal') %>% 
   mutate(goal=play_type_to_goal_boolean(type.text)) %>% 
-  select(-type.text)
-espn_pbps <- espn_pbps %>% 
+  select(-type.text) %>% 
+  mutate(type=encode_shot_type(text))
+
+espn_shots <- espn_pbps %>% 
   filter(type=='Shot' | type=='Goal') %>% 
   mutate(goal=play_type_to_goal_boolean(type)) %>% 
-  select(-type)
+  select(-type) %>% 
+  mutate(type=encode_shot_type(text))
